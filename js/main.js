e2893154d08533cd73475818f36c3b204368656e
Georgiy Filippov <g.filippov777@gmail.com>
2018-11-17 02:44:09 +0500

    add dropdown menu "menu-wrapper"

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var linkMenu = document.querySelector(".link-menu");
 var iconMenu = document.querySelector(".icon-menu");
 var iconXmark = document.querySelector(".icon-xmark");
+var menuWrapper = document.querySelector(".menu-wrapper");
 
 linkMenu.addEventListener("click", function (evt) {
   evt.preventDefault();
   iconMenu.classList.toggle("icon-menu-toggle");
   iconXmark.classList.toggle("icon-xmark-toggle");
+  menuWrapper.classList.toggle("menu-wrapper-toggle");
 });
